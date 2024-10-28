320185a334398be5f4b556c49e634e63c0e161ad
dizhengcindy <dizhengcindy@gmail.com>
2024-10-27 21:15:54 -0500

    custom hooks

---
+++ Section-16-custom-hooks/src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (fetchFn, initialValue) => {
+    const [isFetching, setIsFetching] = useState();
+    const [error, setError] = useState();
+    const [fetchedData, setFetchedData] = useState(initialValue);
+  
+    useEffect(() => {
+        async function fetchPlaces() {
+          setIsFetching(true);
+          try {
+            const data = await fetchFn();
+            setFetchedData(data);
+          } catch (error) {
+            setError({ message: error.message || 'Failed to fetch data.' });
+          }
+    
+          setIsFetching(false);
+        }
+    
+        fetchPlaces();
+      }, [fetchFn]);
+
+      return {isFetching, error, fetchedData, setFetchedData}
+}
--- Section-16-custom-hooks/summary.md
+++ Section-16-custom-hooks/summary.md
@@ -1,2 +1,8 @@
 Hooks can be used inside of components or other hooks
 Hooks cannot be called inside of if statement, must be top level.
+
+246.
+any state managed by custom hook will also belong to the component where custom hook been called. If state in custom hook update the component get executed again. The same as you are managing the states directly in the component. 
+
+247.
+state updating function are guaranteed by React that they will never change
